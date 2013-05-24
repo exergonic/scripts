9d37a1a27e39a23c9a59ba7d3df504566d22d2f6
Billy Wayne McCann <thebillywayne@gmail.com>
2013-05-24 10:59:23 -0400

    completed pipi.py

--- boss/pipi.py
+++ boss/pipi.py
@@ -12,7 +12,8 @@ cutoff = 10.0
 # containing the pi system. The center is approximated by using two of the
 # three coordinates given by the user. The two atoms which will approximate the
 # center need to be the second and third atoms defined in the lists below. The
-# atoms are defined by the atom labels as they appear in the pdb file.
+# atoms are defined by the atom labels as they appear in the pdb file. Please
+# note, this script works with pdb files ONLY.
 
 solute_atoms = ['S00', 'C01', 'C04']
 solvent_atoms = ['C05', 'N04', 'N06']
@@ -23,19 +24,29 @@ solvent_atoms = ['C05', 'N04', 'N06']
 # The script will find the angles between the planes involved in the (hopefully
 # found) pi-pi stacking, as well as take an average and standard deviation.
 # Output for every solvent molecule evaluated is printed in an output file
-# named 'angles.txt'.
+# named 'output_pi-pi.txt'.
+# 
+# !!The above two variables should be all that is required for the user to
+# change!!
 #
 ## INVOCATION #################################################################
 # 
 # Ensure that the script is marked executable or explicitly invoke python
 # (version 3.2 minimal) to run the script.  Any pdb file which you'd like to
-# analyze should be given as an argument.  
+# analyze should be given as an argument. Shell expansion is handled
+# appropriately.
 # 
+# Example:
+# ./pipi.py *pdb
+# ./pipi.py d50plt5 d50plt10 d50plt15
+#
+# Note that the filenames do not need a pdb suffix, but the script relies on
+# the pdb format.  
 ##############################################################################
 #
 # Author: Billy Wayne McCann
 # email : thebillywayne@gmail.com
-#
+# NOTE:  My code is purposefully verbose. don't hate. 
 ###############################################################################
 
 
@@ -49,7 +60,7 @@ if float(get_python_version()) < 3.2:
     exit(0)
 
 
-# make sure arguments are given to the script
+# make sure arguments have been given to the script
 if len(argv[1:]) == 0:
     print("Give pdb files as an argument to this script.")
     exit(0)
@@ -58,11 +69,10 @@ else:
 
 
 # species are separated in the pdb file by 'TER   '
-# assigning it a variable is easier than using 'TER  ' every time
 ter = 'TER   '
 
 # file in which to place output
-output = open('angles.txt', 'w')
+output = open('output_pi-pi.txt', 'w')
 
 ###  FUNCTIONS  ###############################################################
 
@@ -87,7 +97,7 @@ def get_coordinates(species, atoms):
     atom_arrays = []
     coordinates = []
 
-    # scan through species for specific atom entry atom_arrays
+    # scan through species for specific atom entry and store into atom_arrays
     for entry in species:
         for atom in atoms:
             if atom in entry:
@@ -100,10 +110,16 @@ def get_coordinates(species, atoms):
         coordinates.append(array[4])
         coordinates.append(array[5:8])
 
+    # in binary solvents, the coordinates will sometimes not be found in a
+    # particular solvent molecule. return None and test for it in the main
+    # body.
     if not coordinates:
         return [None,None] 
 
+    # the molecular id is the first entry in the coordinates list
     mol_id = coordinates[0]
+
+    # the actual coordinates are the even entries in the coordinates list
     coordinates = [coordinates[i] for i in range(len(coordinates)) if float(i)
                     % 2 != 0]
     return mol_id, coordinates
@@ -175,32 +191,43 @@ def normal(coordinates):
     ''' Given three coordinates, find the normal to the plane created by the
     three coordinates'''
 
-    vector1 = unit(vectorize(coordinates[0], coordinates[1]))
-    vector2 = unit(vectorize(coordinates[1], coordinates[2]))
+    vector1 = vectorize(coordinates[0], coordinates[1])
+    vector2 = vectorize(coordinates[1], coordinates[2])
 
     return crossproduct(vector1, vector2)
 
 def calculate_angle(normal1, normal2):
     ''' Calculate the angle between two planes.'''
 
+    # make normals into unit vectors first
+    normal1 = unit(normal1)
+    normal2 = unit(normal2)
+
     # the angle between the two planes of atomic coordinates
     # is the arccosine of dot product of the two normals.
     return  degrees(acos(dotproduct(normal1, normal2)))
 
 
-def stddev(all_angles, average):
+def stddev(all_values, average_value):
     '''Find the standard deviation of the angles'''
 
     # create a new list which is the difference between the average of the
-    # angles and each individual angle.
-    square_diff_from_average = [pow((i-average), 2) for i in all_angles]
+    # values and each individual value.
+    square_diff_from_average = [pow((i-average_value), 2) for i in all_values]
 
     # the standard deviation is square root of the sum of the differences
-    # between each each angle and the average squared divided by the number of
-    # values in the list.
+    # between each each value and the average squared divided by the number of
+    # values in the list ('population' standard deviation).
     return sqrt(sum(square_diff_from_average)/len(square_diff_from_average))
 
+
 ## MAIN  ######################################################################
+
+# initialize dictionary to store solvent data into
+solvent_data = {} 
+
+print("Using a cutoff of {0} Angstroms.".format(cutoff))
+output.write("Using a cutoff of {0} Angstroms.\n\n".format(cutoff))
 
 for pdb in pdbs:
     with open(pdb, 'r') as pdb_file:
@@ -218,35 +245,60 @@ for pdb in pdbs:
             solvent = contents[TERlines[i]:TERlines[i+1]]
             solvents.append(solvent)
 
-        angles = []
+        # keep track of how many solvents are within cutoff in each pdb
+        j = 0
         for solvent in solvents:
             solvent_name, solvent_coordinates = get_coordinates(solvent, solvent_atoms)
             
             if solvent_coordinates is None:
                 continue
 
+            solvent_name = int(solvent_name)
             solvent_center = center(solvent_coordinates[1],
                                     solvent_coordinates[2])
-            solvent_normal = normal(solvent_coordinates)
             radius = distance(solute_center, solvent_center)
 
             if radius < cutoff:
+                solvent_normal = normal(solvent_coordinates)
                 angle = calculate_angle(solute_normal, solvent_normal)
-                angles.append(angle)
-                output.write("Solvent ID: {0}\t".format(solvent_name))
-                output.write("Distance  : {0:.2f}\t".format(radius))
-                output.write("Angle: {0:.2f}\n".format(angle))
+                if solvent_name not in solvent_data:
+                    solvent_data[solvent_name] = {'pdb': [], 
+                            'distance': [], 'angle': []}
+                solvent_data[solvent_name]['pdb'].append(pdb)
+                solvent_data[solvent_name]['distance'].append(radius)
+                solvent_data[solvent_name]['angle'].append(angle)
+                j += 1
+
             else:
                 continue
 
-        angle_average = sum(angles)/len(angles)
-        standard_dev = stddev(angles, angle_average)
+        print("PDB {0} contains {1} solvent(s) within the cutoff.".format(pdb, j))
+        output.write("PDB {0} contains {1} solvent(s) within the cutoff.\n".format(pdb, j))
 
-        output.write('==============================================\n')
-        output.write("Angle average is {0:.2f} +- ".format(angle_average))
-        output.write("{0:.2f}.\n".format(standard_dev))
-        print("Angle average is {0:.2f} +- {1:.2f}".format(angle_average,
-                                                            standard_dev))
-       
+# end of for pdb loop
+
+output.write("\n")
+
+
+# analyze the accepted solvent data.
+for accepted in sorted(solvent_data):
+    output.write('Solvent ID: {0}\n'.format(accepted))
+
+    for index in range(len(solvent_data[accepted]['pdb'])):
+        output.write('+ PDB name: {0}\t\t'.format(solvent_data[accepted]['pdb'][index]))
+        output.write('Distance: {0:.2f}\t'.format(solvent_data[accepted]['distance'][index]))
+        output.write('Angle: {0:.2f}\t\n'.format(solvent_data[accepted]['angle'][index]))
+
+    output.write('== AVERAGES ==\n')
+    average_distance = sum(solvent_data[accepted]['distance'])/len(solvent_data[accepted]['distance'])
+    distance_stddev = stddev(solvent_data[accepted]['distance'],
+                                average_distance)
+    average_angle = sum(solvent_data[accepted]['angle'])/len(solvent_data[accepted]['angle'])
+    angle_stddev = stddev(solvent_data[accepted]['angle'], average_angle)
+    output.write("Distance:\t{0:.2f} +- ".format(average_distance))
+    output.write("{0:.2f}.\n".format(distance_stddev))
+    output.write("Angle:  \t{0:.2f} +- ".format(average_angle))
+    output.write("{0:.2f}.\n\n".format(angle_stddev))
+
 output.close()
 exit(0)
