18733050885c5036be4c84f20eb6690bc5c12699
Billy Wayne McCann <thebillywayne@gmail.com>
2014-06-09 22:36:29 -0400

    compile c

--- compile.sh
+++ compile.sh
@@ -2,15 +2,18 @@
 
 src=${1}
 out=${src%.*}.o
+[[ -e $out ]] && builtin rm $out
 
 fortoptions="-Wall -Wextra -Wimplicit-interface -Werror \
     -O3 -march=native -ffast-math -funroll-loops"
 
 case ${src##*.} in
 	f|f90|f03|f95|F)
-		[[ -e $output ]] && builtin rm $output 
 		gfortran $fortoptions $src -o $out && \
             echo "compile successful: $out produced"
+		;;
+	c)
+		gcc $src -o $out 
 		;;
 	*)
 		echo "Don't know how to do that yet"
