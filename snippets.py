c59e4963a441c8130a1158c60fb4fa3132b33ea9
Billy Wayne McCann <thebillywayne@gmail.com>
2014-11-13 16:28:41 -0500

    python snippets

---
+++ snippets.py
@@ -0,0 +1,7 @@
+def return_interval(string, delim1, delim2):
+    '''
+    Return the range in a string that is delimited by delim1 and delim2
+    '''
+
+    return(string.find(delim1), string.find(delim2))
+
